791a36e360051384888cb687888e2539e87c70bf
Domuserk <rgdesenhos321@gmail.com>
2021-05-20 17:27:06 -0300

    new changes

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
@@ -6,8 +7,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params; 
+    try {
+     const user = this.showUserProfileUseCase.execute({user_id})
+     return response.json(user)
+    }catch(err) {
+     return response.status(404).json({ error: err.message });
   }
+ }
 }
 
 export { ShowUserProfileController };
